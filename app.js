1599af71bf73b7c7ec3aabe082b11aaf03ddfdeb
Rayyan Suhail <suhailr@mcmaster.ca>
2023-07-17 00:04:28 +0300

    Basic CSS addition

--- app.js
+++ app.js
@@ -2,26 +2,28 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-var items = [];
+let items = [];
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.listen(3000, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3000 ");
 });
 
 app.get("/", function (req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
   res.render("list", { kindOfDay: day, itemList: items });
 });
 
