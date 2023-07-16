f906956191354568eb940c06721fc53d1f172219
Rayyan Suhail <suhailr@mcmaster.ca>
2023-07-16 23:15:14 +0300

    New task addition feature

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
   };
 
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newListItem: items });
+  res.render("list", { kindOfDay: day, itemList: items });
 });
 
 app.post("/", function (req, res) {
