6d1e468711e9bd65826905a11a9f5cc3c3f20175
Rayyan Suhail <suhailr@mcmaster.ca>
2023-07-16 20:08:11 +0300

    Added standardized date format

--- app.js
+++ app.js
@@ -14,23 +14,13 @@ app.listen(3000, function () {
 
 app.get("/", function (req, res) {
   var today = new Date();
-  const currentDay = today.getDay();
-  var day = "";
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
 
-  //   if (currentDay == 6 || currentDay == 0) {
-  //     day = "Weekend";
-  //   } else {
-  //     day = "Weekday";
-  //   }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  res.render("list", { kindOfDay: daysOfWeek[currentDay] });
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day });
 });
